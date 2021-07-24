ee0eba92ed9bec4e234193abc47eaf1e2e61f003
candidate <candidate@programmr.com>
2021-07-24 10:19:23 +0000

    submit

--- Challenge.java
+++ Challenge.java
@@ -7,17 +7,14 @@ public class Challenge {
 		Scanner scanner = new Scanner(System.in); 
 		System.out.println("What is your name: "); 
 		String name = scanner.next();
-		String echo = ""; 
+		
 		///{ 
 	 
 		//start your coding here  
      
-        //echo = "Hello "+name;
-           
-
 		///}   
  
-                System.out.println(echo);
+                System.out.println(new StringBuilder(name).reverse().toString());
                 scanner.close();
 	} 
 } 
